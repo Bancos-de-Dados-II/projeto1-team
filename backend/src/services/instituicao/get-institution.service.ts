2a161be725f17f669c5744fe7b2cd022f55dedcf
ThamiresAntunes <antunesthamires8@gmail.com>
2025-06-14 19:20:02 -0300

    fix: corrigindo o service de listar

--- backend/src/services/instituicao/get-institution.service.ts
+++ backend/src/services/instituicao/get-institution.service.ts
@@ -1,9 +1,14 @@
 import prisma from "../../prisma/client";
 import { Institution } from "../../@types/instituicao";
 
-export async function getInstitutionService() : Promise<Institution[] | null> {
-    const list = await prisma.$queryRaw<Institution[]>`
-        SELECT * FROM "Instituicao"
+export async function getInstitutionService(): Promise<Institution[] | null> {
+  const list = await prisma.$queryRaw<Institution[]>`
+    SELECT "id", 
+        "name",
+        "cnpj",
+        "contact",
+        "description"
+    FROM "Instituicao"
     `;
-    return list.length > 0 ? list : null;
+  return list.length > 0 ? list : null;
 }
