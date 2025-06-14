aaac5bd52051013213a0e0df36da185af36a2539
ThamiresAntunes <antunesthamires8@gmail.com>
2025-06-14 15:32:42 -0300

    feat: criando a router de post de instituicao

--- backend/src/routes/institution.router.ts
+++ backend/src/routes/institution.router.ts
@@ -1,2 +1,8 @@
+import { Router } from "express";
+import { createInstitutionController } from "../controllers/institution/create-institution.controller";
 
+const router = Router();
 
+router.post("/", createInstitutionController);
+
+export default router;
