ed117a60f838f41d48bab38241e0cc689d16931c
Francisco <francisco.andraade16@gmail.com>
2025-06-16 03:47:01 -0300

    Front (Leaflet, Cors, JS, html, css, nominatim)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import cors from "cors";
 import institutionRouter from "./routes/institution.router";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use("/institutions", institutionRouter);
